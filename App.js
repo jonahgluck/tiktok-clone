b09f6b5004d1c946f60f706c9d1ede651ad5e552
Jonah <jonah11104@gmail.com>
2020-12-27 11:30:57 -0500

    add firebase auth

--- App.js
+++ App.js
@@ -4,17 +4,62 @@
  */
 
 import React from 'react';
-import {StatusBar} from 'react-native';
+import {SafeAreaView, StatusBar, View} from 'react-native';
+import firebase from 'firebase';
 import 'react-native-gesture-handler';
 import RootNavigation from './src/navigation/index';
+import LoginForm from './src/auth/LoginForm';
+import Profile from './src/auth/Profile';
+import {Header, Spinner} from './src/auth/common';
+export default class App extends React.Component {
+  state = {loggedIn: false};
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <RootNavigation />
-    </>
-  );
-};
+  constructor(props) {
+    super(props);
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBoRomqSQZme4TCKH9sd9ko2kXKV0dfhfA',
+      authDomain: 'tiktok-clone-61075.firebaseapp.com',
+      projectId: 'tiktok-clone-61075',
+      storageBucket: 'tiktok-clone-61075.appspot.com',
+      messagingSenderId: '207562137789',
+      appId: '1:207562137789:web:8770bfa049efaa36b057d4',
+      measurementId: 'G-VPY4EQH1QH',
+    });
 
-export default App;
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({loggedIn: true});
+      } else {
+        this.setState({loggedIn: false});
+      }
+    });
+  }
+
+  renderForm() {
+    if (this.state.loggedIn) {
+      <>
+        <StatusBar barStyle="light-content" />
+        <RootNavigation />
+      </>;
+    } else {
+      return (
+        <>
+          <SafeAreaView>
+            <LoginForm />
+          </SafeAreaView>
+        </>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <SafeAreaView>
+        <View>
+          <Header headerText="Auth" />
+          {this.renderForm()}
+        </View>
+      </SafeAreaView>
+    );
+  }
+}
---
+++ src/auth/Profile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Text} from 'react-native';
+import firebase from 'firebase';
+import {Button, Card, CardSection} from './common';
+
+export default class Profile extends React.Component {
+  onButtonPress() {
+    firebase.auth().signOut();
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Text style={styles.textStyle}>Welcome Ilkin!</Text>
+        </CardSection>
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>Log out</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const styles = {
+  textStyle: {
+    fontSize: 17,
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+};
